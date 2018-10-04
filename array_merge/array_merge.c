50dbd6e6d4950d2fcda9322f9947676982060516
Nick Plucker <pluck011@morris.umn.edu>
2018-10-04 12:05:17 -0500

    Fix problem with loops not removing duplicates and fix memory leaks in array_merge and tests
    All the tests pass and there are no memory leaks.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
 #include "mergesort.c"
 #include "array_merge.h"
 
-
+// Function prototypes
 int* array_merge(int num_arrays, int* sizes, int** values);
 int* removeDuplicates(int* array, int size);
 int* flattenArrayOfArrays(int num_arrays, int* sizes, int** arrayOfArrays, int* flatArray);
 
+
 int* array_merge(int num_arrays, int* sizes, int** values) {
 
+  // Finding the size of the values array
   int sizeOfValues = 0;
   for (int i = 0; i < num_arrays; i++) {
     sizeOfValues = sizeOfValues + sizes[i];
@@ -25,8 +26,10 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
   // sort the result containing duplicates
   mergesort(sizeOfValues, dupResult);
 
+  // removes the duplicates with the size in result[0] followed by the elements
   int* result = removeDuplicates(dupResult, sizeOfValues);
 
+  free(dupResult);
   return result;
 }
 
@@ -36,22 +39,25 @@ int* removeDuplicates(int* array, int size) {
   // start numUniqueValues at 1 since we're assuming the first value to be
   // unique.
   int i;
-  int numUniqueValues = 1;
-  for(i = 1; i < size; i++) {
-    if (array[i] != array[i-1]) {
+  int numUniqueValues = 0;
+  for(i = 0; i < size; i++) {
+    if (array[i] != array[i+1]) {
       numUniqueValues++;
     }
   }
 
   int* result = (int*) calloc(numUniqueValues + 1, sizeof(int));
+
+  // The first element in result is going to be the number of values that precede it
   result[0] = numUniqueValues;
 
   // start resultIndex to account for the size value
   int resultIndex = 1;
 
+  // Remove duplicates from the given array and put it in the results array
   int j;
-  for(j = 1; j < size; j++) {
-    if (array[j] != array[j-1]) {
+  for(j = 0; j < size; j++) {
+    if (array[j] != array[j+1]) {
       result[resultIndex] = array[j];
       resultIndex++;
     }
